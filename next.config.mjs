834f105f94ea0c2751d8e5debb0bd072648693b8
okm <okkarkaungmyat.okm@gmail.com>
2026-09-21 14:57:23 +0700

    docker added

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: "standalone",
+
+  basePath: "/backend",
 };
 
 export default nextConfig;
